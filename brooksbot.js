56db13451975a2ef512741b776f605197394c4f7
bpepy <33704441+bpepy@users.noreply.github.com>
2018-05-22 01:03:05 -0400

    Create brooksbot.js

---
+++ brooksbot.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    switch(message.author.bot) {
+
+	case true:
+	break;
+	case false:
+        if (message.content.substring(0,1) === '!') {
+    	    message.channel.send('HENRIK!');
+  	    }
+            break;
+    }
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
