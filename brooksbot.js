31e99177c5cc657a318dd8c5c370163eda021aff
bpepy <33704441+bpepy@users.noreply.github.com>
2018-05-22 03:21:25 -0400

    Update brooksbot.js

--- brooksbot.js
+++ brooksbot.js
@@ -18,9 +18,18 @@ client.on('message', message => {
 	var quoSize = quo.length;
 	var rand = Math.floor(Math.random() * quoSize);
 
-        if (message.content.substring(0,6) === '-quote') {
-    	    message.channel.send(quo[rand]);
-  	    }
+        if (message.content === '-quote') {
+    	    var type = 1;
+		message.channel.send(type);
+  	}
+	elseif (message.content.substring(0,6) ==='-quote') {
+	    var type = 2;
+		message.channel.send(type);
+	}
+	elseif (message.content.substring(0,4) === '-add') {
+	    var type = 3;
+		message.channel.send(type);
+	}
 	
 });
 
