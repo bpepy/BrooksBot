6fc577d83676411bc20547bdb365197595254c28
bpepy <33704441+bpepy@users.noreply.github.com>
2018-05-22 02:34:55 -0400

    Update brooksbot.js

--- brooksbot.js
+++ brooksbot.js
@@ -6,26 +6,83 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-	var fs = require('fs');
+
+	function wordInString(s, word){
+  return new RegExp( '\\b' + word + '\\b', 'i').test(s);
+}
+var mess=message.contents.toLowerCase();
+
+if (message.contents.substring(0,6) == '-quote') {
+        var args = message.contents.substring(6).split(' ');
+        var cmd = args[0];
+//check quotes 
+var fs = require('fs');
 
 try {  
     var data = fs.readFileSync('quotelog.txt', 'utf8');    
 } catch(e) {}
-	
+
 var quo = data.split(";");
 var quoSize = quo.length;
-var rand=Math.floor(Math.random() * quoSize);
-	
-    switch(message.author.bot) {
 
-	case true:
-	break;
-	case false:
-        if (message.content.substring(0,1) === '!') {
-    	    message.channel.send(quo[rand]);
-  	    }
-            break;
-    }
+
+	if (message.contents.substring(6,7) == ' ') {
+	   var stopcheck = [];
+	   for (rand=Math.floor(Math.random() * quoSize); stopcheck.length < quoSize;rand=Math.floor(Math.random() * quoSize)) {
+		if (stopcheck.includes(rand)==false) {
+			if (wordInString(quo[rand].toLowerCase(),mess.substring(7))) {
+		                    message.channel.send(quo[rand]);
+				stopcheck.splice(0,4);
+				break;
+			}
+			else {
+				stopcheck.splice(0,0,rand);
+			}
+		}
+		else {
+			continue;
+		}
+	   }
+		if (stopcheck.length >= quoSize) {
+	        	message.channel.send('No quote found');
+		}
+	}
+	else {
+		var rand=Math.floor(Math.random() * quoSize);
+	
+	        switch(cmd) {
+	            // !ping
+		        case '':
+	                message.channel.send(quo[rand]);
+	            break;
+            
+	        }
+	}
+}
+if (message.contents.substring(0,4) == '-add') {
+        var args = message.contents.substring(4).split(' ');
+        var cmd = args[0];
+//check quotes 
+var fs = require('fs');
+
+try {  
+    var data = fs.readFileSync('quotelog.txt', 'utf8');    
+} catch(e) {}
+//add quote
+	var fs = require('fs');
+	var newquo = data+';'+message.substring(4);
+		
+	fs.writeFile('quotelog.txt',newquo, 'utf8', function(err, data) {  
+		    if (err) throw err;
+		});
+
+        switch(cmd) {
+            // !ping
+	    case '':
+                message.channel.send('Fake news added');
+	    break;
+	}
+}
 });
 
 // THIS  MUST  BE  THIS  WAY
