560780d1a0961c2673295200d3db1a274a1a2aab
bpepy <33704441+bpepy@users.noreply.github.com>
2018-10-31 14:01:00 -0400

    Update brooksbot.js

--- brooksbot.js
+++ brooksbot.js
@@ -82,7 +82,7 @@ client.on('message', message => {
 	case 4:
 			var minus = parseInt(message.content.substring(5));
 			var last = quoSize - minus;
-			message.channel.send(quo[1]);
+			message.channel.send('minus ='+minus + 'last ='+last);
 	case 5:
 	break;
     	}
